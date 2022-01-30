c301728e945365fb0dc5e73a03b6059b39a2b659
Reuven Tyk <reuventyk@gmail.com>
2022-01-30 15:39:51 +0200

    progress bar working

--- fish.js
+++ fish.js
@@ -1,19 +1,30 @@
 const STARTING_COUNTER_DEFAULT = 0;
-let sec = 0;
+let sec = 0,
+  counter = 0,
+  intervalID;
 
 const timer = document.getElementById("timer"),
   userBtn = document.getElementById("user-btn"),
   bar = document.getElementById("bar");
-let counter = 0;
 
-userBtn.addEventListener("click", up);
+userBtn.addEventListener("click", press);
 
-while (counter < 100) {
-  timerGo();
+function press() {
+  counter += 1;
+  if (sec == 0) {
+    alert("You Got Caught");
+    counter = 0;
+  }
+  if (counter >= 100) {
+    alert("You Won");
+  }
+  progressBar(counter);
 }
 
-function up() {
-  check();
+function progressBar(progress) {
+  bar.style.backgroundColor = "black";
+  console.log(progress);
+  bar.style.width = progress + "%";
 }
 
 function timerGo() {
@@ -21,6 +32,7 @@ function timerGo() {
   if (sec == 4) {
     timer.innerHTML = 0;
     sec = 0;
+    /* clearInterval(intervalID); */
     return;
   }
 
@@ -28,8 +40,22 @@ function timerGo() {
   setTimeout("timerGo()", 1000);
 }
 
-function check() {
-  if (userBtn.addEventListener("click") && sec == 0) {
-    alert("You Got Fished");
+timerGo();
+/* 
+function randomFunction() {
+  let time = Math.random() * 10000;
+  console.log(time);
+  if (time > 4000) {
+    return time;
   }
+  return 4000;
 }
+
+function delay() {
+  timerGo();
+}
+
+function start() {
+  intervalID = setInterval(delay, randomFunction());
+}
+ */
